f574b5176ef8a0f32d6335dce7919e4fd1d4c378
edieyelovesye <135151571+edieyelovesye@users.noreply.github.com>
2025-03-21 22:38:48 -0400

    v11

--- layout.tsx
+++ layout.tsx
@@ -47,18 +47,6 @@ export default function RootLayout({
             gtag('config', 'G-9ME3SSV483');
           `}
         </Script>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-9ME3SSV483`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-9ME3SSV483');
-          `}
-        </Script>
       </body>
     </html>
   )
