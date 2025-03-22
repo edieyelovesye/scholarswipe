50a71cdaa4a8ce01af9597b007028febac2f0898
edieyelovesye <135151571+edieyelovesye@users.noreply.github.com>
2025-03-21 23:04:52 -0400

    v15

--- layout.tsx
+++ layout.tsx
@@ -22,19 +22,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
         
-        {/* Google Analytics Scripts - KEEP THESE */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-9ME3SSV483"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-9ME3SSV483');
-          `}
-        </Script>
+        
       </body>
     </html>
   )
