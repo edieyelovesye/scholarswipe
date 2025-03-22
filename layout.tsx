63ec4ea32e00e3168528ca71bb6d662869b88285
edieyelovesye <135151571+edieyelovesye@users.noreply.github.com>
2025-03-21 22:29:36 -0400

    v10

--- layout.tsx
+++ layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -35,7 +36,18 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
-        <GoogleAnalytics gaId="G-9ME3SSV483" />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-9ME3SSV483`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9ME3SSV483');
+          `}
+        </Script>
       </body>
     </html>
   )
